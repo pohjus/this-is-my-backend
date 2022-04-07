0aa590c86f5602b04f1ad1a6bb71a3704b73ab3b
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-04-07 13:25:51 +0300

    dsd

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 var mysql = require("mysql");
+var cors = require("cors");
 
 const app = express();
+
+// http://localhost:8080/frontend/index.html
+// http://localhost:8080/frontend/
+
+// CORS
+app.use(cors());
+app.use(express.static("public"));
+
 const port = process.env.PORT || 8080;
 
 let config = {
@@ -14,7 +23,7 @@ let config = {
 
 let connection = mysql.createConnection(config);
 
-app.get("/", (req, res) => {
+app.get("/locations", (req, res) => {
   connection.query("select * from location", (err, results) => {
     if (err) throw err;
     res.send(results);
