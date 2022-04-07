cb1a7d14d41272afc67a5b08714231053dff3892
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-04-07 10:23:45 +0300

    Delete: back to basics

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-var mysql = require("mysql");
-require("dotenv").config();
-
-let config = {
-  host: "mydb.tamk.fi",
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
-};
-
-let connection = mysql.createConnection(config);
 
 app.get("/", (req, res) => {
-  connection.query("SELECT * FROM location", (err, results) => {
-    if (err) throw err;
-    res.send(results);
-  });
+  res.send("jklkjlj");
 });
 
 app.listen(port, () => {
