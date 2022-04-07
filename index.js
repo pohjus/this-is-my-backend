80012d025982afcc9689cc3a2f5b9e2cec7b08b8
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-04-07 10:35:05 +0300

    Add: mysql connection.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+require("dotenv").config();
+var mysql = require("mysql");
+
 const app = express();
 const port = process.env.PORT || 8080;
 
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+};
+
+let connection = mysql.createConnection(config);
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  connection.query("select * from location", (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
 });
 
 app.listen(port, () => {
